07c83a66ff231001d0294b81384c590d1b4c6fd6
daniel_rezende <danielestudosweb@gmail.com>
2020-11-19 16:58:51 -0300

    parte 4 projeto semana10

--- semana10/labex/src/components/Home.js
+++ semana10/labex/src/components/Home.js
@@ -3,6 +3,14 @@ import styled from 'styled-components'
 import axios from 'axios'
 import { useHistory } from "react-router-dom";
 
+const ContainerHome = styled.div`
+  width:100vw;height:100vh;
+  background-color:aquamarine;
+  text-align:center; 
+  text-justify:auto;
+    button{width:12em; font-size:40px;  background-color:orange;
+      margin-top:200px;}
+`
 export default function Home() {
   const history = useHistory() 
 
@@ -12,11 +20,10 @@ export default function Home() {
 
   
     return (
-      <>
-        <h1>aqui é a home</h1>
-        <button onClick={irParaCadastro}>Cadastrar para viagem</button>
-        <button onClick={irParaLogin}>login</button>
-        <button onClick={irParaViagens}>viagens disponiveis</button>
-      </>
+      <ContainerHome> 
+        <button onClick={irParaCadastro}>Cadastrar para viagem</button>   <br/>
+        <button onClick={irParaLogin}>login</button>  <br/>
+        <button onClick={irParaViagens}>viagens disponiveis</button>  <br/>
+      </ContainerHome>
     );
   }
